6f33e2b8f5c62e893f233ae6fe79f27d2771760c
MD ARIF HOSSAIN <arif.swfu@outlook.com>
2020-08-13 15:14:30 +0800

    signup and signin page by routes and controllers

--- app.js
+++ app.js
@@ -1,21 +1,24 @@
-const express = require('express');
-const mongoose = require('mongoose');
-require('dotenv').config();
+const express = require('express')
+const mongoose = require('mongoose')
+const authRoutes = require('./routes/authRoutes')
+require('dotenv').config()
 
-const app = express();
+const app = express()
 
 // middleware
-app.use(express.static('public'));
+app.use(express.static('public'))
 
 // view engine
-app.set('view engine', 'ejs');
+app.set('view engine', 'ejs')
 
 // database connection
-const dbURI = process.env.DB_PATH;
+const dbURI = process.env.DB_PATH
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
   .then((result) => app.listen(3000))
-  .catch((err) => console.log(err));
+  .catch((err) => console.log(err))
 
 // routes
-app.get('/', (req, res) => res.render('home'));
-app.get('/items', (req, res) => res.render('items'));
+app.get('/', (req, res) => res.render('home'))
+app.get('/items', (req, res) => res.render('items'))
+
+app.use(authRoutes)
