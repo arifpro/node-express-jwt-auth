2f718b3f75a26b153bf985346ab227a9aa8746db
MD ARIF HOSSAIN <arif.swfu@outlook.com>
2020-08-14 16:50:05 +0800

    mongoose validation

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,26 @@
 const User = require('../models/User')
 
+// handle errors
+const handleErrors = (err) => {
+    console.log(err.message, err.code)
+    let errors = { email: '', password: '' }
+
+    // duplicate error code
+    if (err.code === 11000) {
+        errors.email = 'That email is already registered'
+        return  errors
+    }
+
+    // validation errors
+    if (err.message.includes('user validation failed')) {
+        Object.values(err.errors).forEach(({properties}) => {
+            errors[properties.path] = properties.message
+        })
+    }
+
+    return errors
+}
+
 
 // get controllers
 module.exports.signup_get = (req, res) => {
@@ -12,14 +33,14 @@ module.exports.login_get = (req, res) => {
 
 // post controllers
 module.exports.signup_post = async(req, res) => {
-    const { email, password } = req.body;
+    const { email, password } = req.body
 
     try {
         const user = await User.create({ email, password })
         res.status(201).json(user)
     } catch (err) {
-        console.log(err)
-        res.status(400).send('error, user not created')
+        const errors = handleErrors(err)
+        res.status(400).json({ errors })
     }
 }
 module.exports.login_post = async (req, res) => {
--- models/User.js
+++ models/User.js
@@ -1,16 +1,18 @@
 const mongoose = require('mongoose')
+const { isEmail } = require('validator')
 
 const userSchema = new mongoose.Schema({
     email: {
         type: String,
-        required: true,
-        unique: true,
-        lowercase: true
+        required: [true, 'Please enter an email'],
+        unique: [true, ''],
+        lowercase: true,
+        validate: [ isEmail, 'Please enter a valid email']
     },
     password: {
         type: String,
-        required: true,
-        minLength: 6
+        required: [true, 'Please enter an password'],
+        minlength: [6, 'Minimum password length is 6 characters']
     }
 })
 
