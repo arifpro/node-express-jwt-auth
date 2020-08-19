78a0b7a9a801d67e2e0fd8c1fb45f6bf00ac26c1
MD ARIF HOSSAIN <arif.swfu@outlook.com>
2020-08-19 15:21:47 +0800

    Hashing Passwords

--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const { isEmail } = require('validator');
+const bcrypt = require('bcrypt')
 
 const userSchema = new mongoose.Schema({
     email: {
@@ -23,8 +24,9 @@ userSchema.post('save', function (doc, next) {
 })
 
 // Fire a fuction before doc saved to db
-userSchema.pre('save', function (next) {
-    console.log('user about to be created & saved', this);
+userSchema.pre('save', async function (next) {
+    const salt = await bcrypt.genSalt();
+    this.password = await bcrypt.hash(this.password, salt);
     next();
 })
 
