7e1c38a5a5017f9f207cb0e4738c2af3276cc180
MD ARIF HOSSAIN <arif.swfu@outlook.com>
2020-08-20 00:14:23 +0800

    Cookies Primer

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const authRoutes = require('./routes/authRoutes')
+const cookieParser = require('cookie-parser')
 require('dotenv').config()
 
 const app = express()
@@ -8,6 +9,7 @@ const app = express()
 // middleware
 app.use(express.static('public'))
 app.use(express.json())
+app.use(cookieParser())
 
 // view engine
 app.set('view engine', 'ejs')
@@ -21,5 +23,21 @@ mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCr
 // routes
 app.get('/', (req, res) => res.render('home'))
 app.get('/items', (req, res) => res.render('items'))
+app.use(authRoutes);
+
+// cookies
+app.get('/set-cookies', (req, res) => {
+    // res.setHeader('Set-Cookie', 'newUser=true');
+    res.cookie('newUser', false);
+    res.cookie('isEmployee', true, { maxAge: 1000 * 60 * 60 * 24, httpOnly: true });   //secure: true
+
+    res.send('you got the cookies!')
+})
+app.get('/read-cookies', (req, res) => {
+    const cookies = req.cookies;
+    console.log(cookies.newUser);
+
+    res.json(cookies);
+})
 
 app.use(authRoutes)
