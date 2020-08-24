b91307b5e2188900d5c70fd5959eae4e8f3dfcac
MD ARIF HOSSAIN <arif.swfu@outlook.com>
2020-08-24 12:39:49 +0800

    signup and store jwt in cookies

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.set('view engine', 'ejs')
 // database connection
 const dbURI = process.env.DB_PATH
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
-  .then((result) => app.listen(3000))
+  .then((result) => app.listen(3005))
   .catch((err) => console.log(err))
 
 // routes
@@ -25,19 +25,3 @@ app.get('/', (req, res) => res.render('home'))
 app.get('/items', (req, res) => res.render('items'))
 app.use(authRoutes);
 
-// cookies
-app.get('/set-cookies', (req, res) => {
-    // res.setHeader('Set-Cookie', 'newUser=true');
-    res.cookie('newUser', false);
-    res.cookie('isEmployee', true, { maxAge: 1000 * 60 * 60 * 24, httpOnly: true });   //secure: true
-
-    res.send('you got the cookies!')
-})
-app.get('/read-cookies', (req, res) => {
-    const cookies = req.cookies;
-    console.log(cookies.newUser);
-
-    res.json(cookies);
-})
-
-app.use(authRoutes)
--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,6 @@
-const User = require('../models/User')
+const User = require('../models/User');
+const jwt = require('jsonwebtoken');
+require('dotenv').config()
 
 // handle errors
 const handleErrors = (err) => {
@@ -21,6 +23,15 @@ const handleErrors = (err) => {
     return errors
 }
 
+// token
+const maxAge= 3 * 24 * 60 * 60;
+const createToken = id => {
+    //process.env.SECRET_KEY
+    return jwt.sign({id}, "asdfasdfd", {
+        expiresIn: maxAge
+    });
+};
+
 
 // get controllers
 module.exports.signup_get = (req, res) => {
@@ -36,8 +47,10 @@ module.exports.signup_post = async(req, res) => {
     const { email, password } = req.body
 
     try {
-        const user = await User.create({ email, password })
-        res.status(201).json(user)
+        const user = await User.create({ email, password });
+        const token = createToken(user._id);
+        res.cookie('jwt', token, { httpOnly: true, maxAge: maxAge * 1000 });
+        res.status(201).json({ user: user._id });
     } catch (err) {
         const errors = handleErrors(err)
         res.status(400).json({ errors })
