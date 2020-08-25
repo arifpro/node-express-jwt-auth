97072d26d4ecce16fd04107eb6a758a6bebd6c8f
MD ARIF HOSSAIN <arif.swfu@outlook.com>
2020-08-25 18:26:48 +0800

    login with jwt and save the jwt in cookies

--- controllers/authController.js
+++ controllers/authController.js
@@ -7,6 +7,16 @@ const handleErrors = (err) => {
     console.log(err.message, err.code)
     let errors = { email: '', password: '' }
 
+    // incorrect email
+    if (err.message === 'Incorrect email') {
+        errors.email = 'That email is not registered';
+    }
+    // incorrect password
+    if (err.message === 'Incorrect password') {
+        errors.password = 'That password is incorrect';
+    }
+
+
     // duplicate error code
     if (err.code === 11000) {
         errors.email = 'That email is already registered'
@@ -60,8 +70,11 @@ module.exports.login_post = async (req, res) => {
 
     try {
         const user = await User.login(email, password);
+        const token = createToken(user._id);
+        res.cookie('jwt', token, { httpOnly: true, maxAge: maxAge * 1000 });
         res.status(200).json({ user: user._id })
     } catch (err) {
-        res.status(400).json({})
+        const errors = handleErrors(err);
+        res.status(400).json({ errors });
     }
 }
