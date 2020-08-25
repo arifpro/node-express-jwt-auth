cebc54bc376e70c8f3606b37041f510c8382945f
MD ARIF HOSSAIN <arif.swfu@outlook.com>
2020-08-25 18:16:54 +0800

    login with jwt

--- controllers/authController.js
+++ controllers/authController.js
@@ -58,6 +58,10 @@ module.exports.signup_post = async(req, res) => {
 module.exports.login_post = async (req, res) => {
     const { email, password } = req.body;
 
-    console.log(email, password)
-    res.send('user login')
+    try {
+        const user = await User.login(email, password);
+        res.status(200).json({ user: user._id })
+    } catch (err) {
+        res.status(400).json({})
+    }
 }
