f9e4a1d2580b0c05a7654df4281bae2326cc505c
MD ARIF HOSSAIN <arif.swfu@outlook.com>
2020-08-27 20:58:46 +0800

    Logging Users Out

--- controllers/authController.js
+++ controllers/authController.js
@@ -78,3 +78,8 @@ module.exports.login_post = async (req, res) => {
         res.status(400).json({ errors });
     }
 }
+
+module.exports.logout_get = (req, res) => {
+    res.cookie('jwt', '', { maxAge: 1 });
+    res.redirect('/');
+}
