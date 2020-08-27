513bd5905ffb18a2e7e9a696ee875b94b47f1230
MD ARIF HOSSAIN <arif.swfu@outlook.com>
2020-08-27 21:22:11 +0800

    Checking the Current User

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const authRoutes = require('./routes/authRoutes');
 const cookieParser = require('cookie-parser');
-const { requireAuth } = require('./middleware/authMiddleware');
+const { requireAuth, checkUser } = require('./middleware/authMiddleware');
 require('dotenv').config();
 
 const app = express();
@@ -22,6 +22,7 @@ mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCr
   .catch((err) => console.log(err))
 
 // routes
+app.get('*', checkUser);
 app.get('/', (req, res) => res.render('home'));
 app.get('/items', requireAuth, (req, res) => res.render('items'));
 app.use(authRoutes);
--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -20,4 +20,27 @@ const requireAuth = (req, res, next) => {
     }
 }
 
-module.exports = { requireAuth };
+// check current user
+const checkUser = (req, res, next) => {
+    const token = req.cookies.jwt;
+
+    if (token) {
+        wt.verify(token, process.env.SECRET_KEY, async (err, decodedToken) => {
+            if (err) {
+                console.log(err.message);
+                res.locals.user = null;
+                next();
+            } else {
+                console.log(decodedToken);
+                let user = await User.findById(decodedToken.id);
+                res.locals.user = user;
+                next();
+            }
+        })
+    } else {
+        res.locals.user = null;
+        next();
+    }
+}
+
+module.exports = { requireAuth, checkUser };
